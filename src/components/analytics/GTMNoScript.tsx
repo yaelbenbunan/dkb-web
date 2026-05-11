493fbdc2053b1462a26f7a11c3d7b3f1f38eb2eb
yaelbenbunan <benbunanyael@gmail.com>
2026-05-11 13:43:49 +0200

    feat(analytics): add GTM noscript fallback component

---
+++ src/components/analytics/GTMNoScript.tsx
@@ -0,0 +1,16 @@
+import { GTM_ID } from "@/lib/gtm";
+
+export function GTMNoScript() {
+  if (!GTM_ID) return null;
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+        title="Google Tag Manager"
+      />
+    </noscript>
+  );
+}
